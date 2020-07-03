e0755d54eb8103d1baff2abfe46195ae7bd3d0af
zmxhawrhbg <25387744+Zmxhawrhbg@users.noreply.github.com>
2020-07-02 20:42:45 -0700

    vs compilation fix
    i have to fix compile every release and it's too funny

--- framework.h
+++ framework.h
@@ -2,5 +2,8 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
+#ifndef __MINGW32__
+#define NOMINMAX
+#endif
 #include <limits>
 #include <windows.h>
