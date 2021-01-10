58e08fbec6524f416f02155d5599800d5ed09f65
zmxhawrhbg <25387744+kyurime@users.noreply.github.com>
2021-01-09 17:50:20 -0700

    redo config format

---
+++ src/config_defaults.hpp
@@ -0,0 +1,142 @@
+#pragma once
+#ifndef CONFIG_DEFAULTS_HPP
+#define CONFIG_DEFAULTS_HPP
+
+#include <toml.hpp>
+
+namespace Config {
+
+constexpr int LATEST_VERSION = 3;
+constexpr auto DEFAULT_EXECUTABLE = "GeometryDash.exe";
+constexpr auto DEFAULT_URL = "http://boomlings.com";
+
+struct Presence {
+  std::string detail;
+  std::string state;
+  std::string smalltext;
+
+  void from_toml(const toml::value &table) {
+    this->detail = toml::find<std::string>(table, "detail");
+    this->state = toml::find<std::string>(table, "state");
+    this->smalltext = toml::find<std::string>(table, "smalltext");
+  }
+
+  toml::value into_toml() const {
+    return toml::table{{"detail", this->detail},
+                       {"state", this->state},
+                       {"smalltext", this->smalltext}};
+  }
+};
+
+struct Config_Format {
+  struct Level {
+    Config::Presence saved;
+    Config::Presence playtesting;
+    Config::Presence error;
+
+    void from_toml(const toml::value &table) {
+      this->saved = toml::find<Config::Presence>(table, "saved");
+      this->playtesting = toml::find<Config::Presence>(table, "playtesting");
+      this->error = toml::find<Config::Presence>(table, "error");
+    }
+
+    toml::value into_toml() const {
+      return toml::table{{"saved", this->saved},
+                         {"playtesting", this->playtesting},
+                         {"error", this->error}};
+    }
+  };
+
+  struct Editor : Config::Presence {
+    bool reset_timestamp;
+
+    void from_toml(const toml::value &table) {
+      this->detail = toml::find<std::string>(table, "detail");
+      this->state = toml::find<std::string>(table, "state");
+      this->smalltext = toml::find<std::string>(table, "smalltext");
+      this->reset_timestamp =
+          toml::find_or<bool>(table, "reset_timestamp", reset_timestamp);
+    }
+
+    toml::value into_toml() const {
+      return toml::table{{"detail", this->detail},
+                         {"state", this->state},
+                         {"smalltext", this->smalltext},
+                         {"reset_timestamp", this->reset_timestamp}};
+    }
+  };
+
+  struct User {
+    std::string ranked;
+    std::string default;
+    bool get_rank;
+
+    void from_toml(const toml::value &table) {
+      this->ranked = toml::find<std::string>(table, "ranked");
+      this->default = toml::find<std::string>(table, "default");
+      this->get_rank = toml::find<bool>(table, "get_rank");
+    }
+
+    toml::value into_toml() const {
+      return toml::table{{"ranked", this->ranked},
+                         {"default", this->default},
+                         {"get_rank", this->get_rank}};
+    }
+  };
+
+  struct Settings {
+    int file_version;
+    bool logging;
+    std::string executable_name;
+    std::string base_url;
+
+    void from_toml(const toml::value &table) {
+      this->file_version = toml::find<int>(table, "file_version");
+      this->logging = toml::find<bool>(table, "logging");
+      this->executable_name = toml::find_or<std::string>(
+          table, "executable_name", DEFAULT_EXECUTABLE);
+      this->base_url =
+          toml::find_or<std::string>(table, "base_url", DEFAULT_URL);
+    }
+
+    toml::value into_toml() const {
+      return toml::table{{"file_version", this->file_version},
+                         {"logging", this->logging},
+                         {"executable_name", this->executable_name},
+                         {"base_url", this->base_url}};
+    }
+  };
+
+  void from_toml(const toml::value &table) {
+    this->level = toml::find<Config_Format::Level>(table, "level");
+    this->editor = toml::find<Config_Format::Editor>(table, "editor");
+    this->user = toml::find<Config_Format::User>(table, "user");
+    this->menu = toml::find<Config::Presence>(table, "menu");
+
+    if (table.contains("settings")) {
+      // this table is still optional due to previous versions not containing it
+      this->settings = toml::find<Config_Format::Settings>(table, "settings");
+    }
+  }
+
+  toml::value into_toml() const {
+    return toml::table{{"level", this->level},
+                       {"editor", this->editor},
+                       {"user", this->user},
+                       {"menu", this->menu},
+                       {"settings", this->settings}};
+  }
+
+  Level level = {
+      {"Playing {name}", "by {author} ({best}%)", "{stars}* {diff} ({id})"},
+      {"Editing a level", "", ""},
+      {"Playing a level", "", ""}};
+  Editor editor = {{"Editing a level", "", ""}, false};
+  User user = {"{name} [Rank #{rank}]", "", true};
+  Config::Presence menu = {"Idle", "", ""};
+
+  Settings settings = {Config::LATEST_VERSION, false, Config::DEFAULT_EXECUTABLE, Config::DEFAULT_URL};
+};
+} // namespace Config
+
+#endif
--- src/game_loop.hpp
+++ src/game_loop.hpp
@@ -2,6 +2,8 @@
 #include "gdapi.hpp"
 #include "pch.h"
 #include "presence_wrapper.hpp"
+#include "config_defaults.hpp"
+
 #include <algorithm>
 #include <ctime>
 #include <functional>
@@ -22,24 +24,6 @@ enum class playerState {
   menu,
 };
 
-struct configPresence {
-  std::string detail;
-  std::string state;
-  std::string smalltext;
-
-  void from_toml(const toml::value &table) {
-    this->detail = toml::find<std::string>(table, "detail");
-    this->state = toml::find<std::string>(table, "state");
-    this->smalltext = toml::find<std::string>(table, "smalltext");
-  }
-
-  toml::table into_toml() const {
-    return toml::table{{"detail", this->detail},
-                       {"state", this->state},
-                       {"smalltext", this->smalltext}};
-  }
-};
-
 class Game_Loop {
 private:
   playerState player_state;
@@ -53,14 +37,9 @@ private:
   time_t current_timestamp;
   Discord_Presence *discord;
 
-  configPresence saved_level, playtesting_level, error_level, editor_status,
-      menu_status;
+  Config::Config_Format config;
+
   std::string large_text;
-
-  bool editor_reset_timestamp, output_logging, get_rank;
-  std::string user_ranked, user_default;
-
-  std::string executable_name, base_url;
 
   // wrapper to presence update that allows strings + debug messages
   void update_presence_w(std::string &, std::string &, std::string &,
